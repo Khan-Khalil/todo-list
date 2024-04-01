841a3cc5bc1de04ae4418d942c3c153243e48762
Khalil Khan <khalilahmad.me@gmail.com>
2024-04-01 15:53:13 +0200

    sign out

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,30 @@
-import React from "react"
+import { signOut, onAuthStateChanged } from "firebase/auth"
+import React, { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { auth } from "../firebase"
 
 export default function Homepage() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if(!user){
+                navigate('/')
+            }
+        })
+    },[])
+
+    const handleSignout = () => {
+        signOut(auth).then(() => {
+            navigate('/')
+        }).catch((err) => {
+            alert(err.message)
+        })
+    }
     return (
         <div>
             <h1>homepage</h1>
+            <button onClick={handleSignout}>Sign out</button>
         </div>
     )
 }
